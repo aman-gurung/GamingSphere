80d2dd9f446cd6cc66698df2542324fb090d8bad
ItsATG <anupsingh.1997@gmail.com>
2018-07-02 08:47:04 +0530

    here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'products', component: ProductsComponent },
+
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
